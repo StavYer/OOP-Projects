bef0a87a92b6c5b4666a5433467d6b4c34f3df38
stav.yermiyahu <stav.yermiyahu@post.runi.ac.il>
2024-06-29 18:29:55 +0300

    Added Memory Game App

---
+++ MemoryGame/Board.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryGame
+{
+    internal class Board<T>
+    {
+        private Cell<T>[,] cells;
+       
+        public int Rows
+        {
+            get;
+            private set;
+        }
+
+        public int Columns
+        {
+            get;
+            private set;
+        }
+
+        private Random m_Random;
+
+        private T[] contentArray;
+
+        private List<Cell<T>> unflippedTiles;  
+
+        public Board(int io_Rows, int io_Columns)
+        {
+            Rows = io_Rows;
+            Columns = io_Columns;
+            cells = new Cell<T>[io_Rows, io_Columns];
+            unflippedTiles = new List<Cell<T>>();
+            m_Random = new Random();
+        }
+
+        public void InitializeBoard()
+        {
+            
+            char startLetter = 'A';
+            contentArray = new T[Rows * Columns / 2];
+
+            for (int i = 0; i < contentArray.Length; i++)
+            {
+                contentArray[i] = (T)(object)startLetter;
+                startLetter++;
+            }
+
+            for (int i = 0; i < Rows * Columns / 2; i++)
+            {
+                PlaceRandomly(contentArray[i]);
+                PlaceRandomly(contentArray[i]);
+            }
+        }
+
+        private void PlaceRandomly(T io_Content)
+        {
+            int row, column;
+
+            do
+            {
+                row = m_Random.Next(Rows);
+                column = m_Random.Next(Columns);
+            } while (cells[row, column] != null);
+
+            cells[row, column] = new Cell<T>(io_Content, column, row);
+
+            unflippedTiles.Add(cells[row, column]);  
+        }
+
+        public Cell<T> GetRandomUnflippedTile()
+        {           
+            int randomUnflippedIndex = m_Random.Next(unflippedTiles.Count);
+            Cell<T> unflippedTile = unflippedTiles[randomUnflippedIndex];
+
+            return unflippedTile;
+        }
+       
+
+        public T GetCellContent(int io_Row, int io_Column)
+        {
+            
+            return cells[io_Row, io_Column].Content;
+        }
+
+        public bool IsCellRevealed(int io_Row, int io_Column)
+        {
+
+            return cells[io_Row, io_Column].IsRevealed;
+        }
+
+        public void RevealCell(Cell<T> io_Cell)
+        {
+            io_Cell.IsRevealed = true;
+            unflippedTiles.Remove(io_Cell); 
+        }
+
+        public void HideCell(Cell<T> io_Cell)
+        {
+            io_Cell.IsRevealed = false;
+            unflippedTiles.Add(io_Cell);
+        }
+
+        public Cell<T> GetSpecificCell(int io_Row, int io_Column)
+        {
+            Cell<T> resultCell = null;
+
+            if ((io_Row >= 0 && io_Row < Rows) && (io_Column >= 0 && io_Column < Columns))
+            {
+                resultCell = cells[io_Row, io_Column];
+            }
+
+            return resultCell;
+        }
+
+        public bool IsGameOver()
+        {
+            bool gameOver = true;
+            foreach (var cell in cells)
+            {
+                if (!cell.IsRevealed)
+                {
+                    gameOver = false;
+                    break;
+                }
+            }
+
+            return gameOver;
+        }
+    }
+}
---
+++ MemoryGame/Cell.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryGame
+{
+    internal class Cell<T>
+    {
+        public T Content 
+        { 
+            get; 
+            private set; 
+        }
+        public bool IsRevealed 
+        { 
+            get; 
+            set; 
+        }
+        public int Row
+        {
+            get; private set;
+        }
+        public int Column
+        {
+            get; private set;
+        }
+
+        public Cell(T content, int column, int row)
+        {
+            Row = row;
+            Column = column;
+            Content = content;
+            IsRevealed = false;
+        }
+
+        public override string ToString()
+        {
+
+            return IsRevealed ? Content.ToString() : " ";
+        }
+    }
+}
